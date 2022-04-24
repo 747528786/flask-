ea9e07236c94e6569e0b961c5d8cc0d3d36df509
747528786 <66994895+747528786@users.noreply.github.com>
2022-04-24 17:21:50 +0800

    Add files via upload

---
+++ flaskproject/static/js/ec_left2.js
@@ -0,0 +1,46 @@
+var ec_left2 = echarts.init(document.getElementById('l2'),'dark');
+
+var ec_left2_option = {
+    title:{
+        text:'门店类型',
+        color: 'white'
+    },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '8%',
+    left: 'center',
+      color:'white'
+  },
+    color:['#F0F78B','#1DAE7F','#334087','#8AC6D0','#4B84A1'],
+  series: [
+    {
+      name: '门店类型',
+      type: 'pie',
+      radius: ['45%', '70%'],
+      avoidLabelOverlap: false,
+      itemStyle: {
+        borderRadius: 10,
+        borderColor: '#333',
+        borderWidth: 0
+      },
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: '40',
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: []
+    }
+  ]
+};
+ec_left2.setOption(ec_left2_option)
